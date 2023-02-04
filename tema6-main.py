2619e6593e7690c3847cae68bfc321df31cae0a3
IDGS-803-20001577 <20001577@alumnos.utleon.edu.mx>
2023-02-03 23:43:37 -0600

    Trabajo del 03 febrero 2023

---
+++ tema6-main.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app= Flask(__name__)
+
+@app.route("/multiplicar")
+def multiplicar():
+
+    return render_template("multipicar.html")
+
+@app.route("/resultado",methods=["POST"])
+def resultado():
+    n1=request.form.get("txtNum1")
+    n2=request.form.get("txtNum2")
+    res=int(n1)*int(n2)
+    return render_template("resultado.html", n1=n1,n=n2,res=res)
+
+if __name__=="_main_":
+    app.run(debug=True)
